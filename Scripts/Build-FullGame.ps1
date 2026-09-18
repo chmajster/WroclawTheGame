5e454c836f9333a088ea970c82fd0e6009d932fa
chmajster <44300091+chmajster@users.noreply.github.com>
2026-09-18 16:37:19 +0200

    Pass offline building archive through full-game build

--- Scripts/Build-FullGame.ps1
+++ Scripts/Build-FullGame.ps1
@@ -2,7 +2,8 @@
 param(
     [Parameter(Mandatory=$true)][string]$EngineRoot,
     [string]$Python='python',
-    [switch]$PrepareOnly
+    [switch]$PrepareOnly,
+    [string]$OfficialBuildingsArchive
 )
 $ErrorActionPreference='Stop'
 Set-StrictMode -Version Latest
@@ -15,6 +16,7 @@ $Args=@{
     Campaign=$true
     OfficialBuildings=$true
 }
+if($OfficialBuildingsArchive){$Args.OfficialBuildingsArchive=$OfficialBuildingsArchive}
 if(-not $PrepareOnly){$Args.Package=$true}
 
 & (Join-Path $PSScriptRoot 'Build-Geography.ps1') @Args
