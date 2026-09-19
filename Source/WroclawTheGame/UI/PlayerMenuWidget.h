c5ddc14515da73c377d6f31561d9580bff92bd91
chmajster <44300091+chmajster@users.noreply.github.com>
2026-09-19 02:55:36 +0200

    Avoid UUserWidget Padding name collision

--- Source/WroclawTheGame/UI/PlayerMenuWidget.h
+++ Source/WroclawTheGame/UI/PlayerMenuWidget.h
@@ -83,7 +83,7 @@ private:
     class UTextBlock* MakeText(const FString& Value, int32 Size = 16, bool bBold = false,
                                FLinearColor Color = FLinearColor::White);
     class UButton* MakeButton(const FString& Label, bool bAccent = false);
-    class UBorder* MakeCard(const FMargin& Padding = FMargin(18));
+    class UBorder* MakeCard(const FMargin& CardPadding = FMargin(18));
     class UBorder* MakeKeycap(const FString& Label);
     class UBorder* MakeInfoRow(const FString& Title, const FString& Subtitle, bool bHighlighted = false);
     void BuildShell();
