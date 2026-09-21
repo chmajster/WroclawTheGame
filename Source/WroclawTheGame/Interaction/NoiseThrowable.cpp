9087f1132aba88d38206b03326d6b18953029667
chmajster <44300091+chmajster@users.noreply.github.com>
2026-09-21 16:01:10 +0200

    assets: replace throwable sphere proxy with rusted can model

--- Source/WroclawTheGame/Interaction/NoiseThrowable.cpp
+++ Source/WroclawTheGame/Interaction/NoiseThrowable.cpp
@@ -2,26 +2,32 @@
 #include "Systems/NoiseSystem.h"
 #include "Engine/World.h"
 #include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
 #include "UObject/ConstructorHelpers.h"
 #include "Engine/StaticMesh.h"
 #include "Perception/AISense_Hearing.h"
 #include "Audio/SliceAudio.h"
 ANoiseThrowable::ANoiseThrowable()
 {
+    Collision = CreateDefaultSubobject<USphereComponent>(TEXT("PhysicsCollision"));
+    RootComponent = Collision;
+    Collision->SetSphereRadius(7.0f);
+    Collision->SetCollisionProfileName(TEXT("PhysicsActor"));
+    Collision->SetSimulatePhysics(true);
+    Collision->SetNotifyRigidBodyCollision(true);
+    Collision->SetCanEverAffectNavigation(false);
+
     Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ThrownObject"));
-    RootComponent = Mesh;
-    static ConstructorHelpers::FObjectFinder<UStaticMesh> Sphere(TEXT("/Engine/BasicShapes/Sphere.Sphere"));
-    Mesh->SetStaticMesh(Sphere.Object);
-    Mesh->SetWorldScale3D(FVector(.1));
-    Mesh->SetCollisionProfileName(TEXT("PhysicsActor"));
-    Mesh->SetSimulatePhysics(true);
-    Mesh->SetNotifyRigidBodyCollision(true);
+    Mesh->SetupAttachment(Collision);
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> CanMesh(TEXT("/Game/FreeModels/PolyHaven/can_rusted/can_rusted_1k.can_rusted_1k"));
+    Mesh->SetStaticMesh(CanMesh.Object);
+    Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
     Mesh->SetCanEverAffectNavigation(false);
 }
 void ANoiseThrowable::BeginPlay()
 {
     Super::BeginPlay();
-    Mesh->OnComponentHit.AddDynamic(this, &ANoiseThrowable::Hit);
+    Collision->OnComponentHit.AddDynamic(this, &ANoiseThrowable::Hit);
     SetLifeSpan(12);
 }
 void ANoiseThrowable::Hit(UPrimitiveComponent *Component, AActor *Other, UPrimitiveComponent *OtherComponent,
